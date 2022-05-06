ec43281e4d39b167b4d5faad5f1e4be11ef1d4bf
Andrea Dragotta <dev@andreadev.it>
2022-05-06 17:06:50 +0200

    updated org files

--- lua/user/org.lua
+++ lua/user/org.lua
@@ -2,6 +2,11 @@
 require('orgmode').setup_ts_grammar()
 
 require('orgmode').setup({
-  org_agenda_files = {'~/Dropbox/org/*', '~/org/**/*'},
-  org_default_notes_file = '~/Dropbox/org/notes.org',
+    org_agenda_files = {'~/Dropbox/org/*', '~/org/**/*'},
+    org_default_notes_file = '~/Dropbox/org/notes.org',
+    mappings = {
+        org = {
+            org_toggle_checkbox = "<leader>oc"
+        }
+    }
 })
